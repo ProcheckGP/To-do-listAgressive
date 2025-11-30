8e61441824844ce767f007b08f7e090962bf6e24
ProcheckGP <andrey.39.con@gmail.com>
2025-11-30 18:11:01 +0300

    add style for dashboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -10,14 +10,21 @@ if (!isset($_SESSION['user_id'])) {
 
 <body>
     <section>
-        <div class="row" style="height: 100vh;">
+        <div class="row">
             <div class="col-2 left-half">
                 <div class="menu-top">
                     здесь разные пункты будут
                 </div>
                 <div class="menu-bottom">
-                    <h4><?php echo $_SESSION['username']; ?></h4>
-                    <a href="/To-do-listAgressive/router.php?action=logout">Выйти</a>
+                    <div class="row">
+                        <div class="col-8 text-center">
+                            <img src="/To-do-listAgressive/view/resources/image/user.png" alt="" height="40px">
+                            <p><?php echo $_SESSION['username']; ?></p>
+                        </div>
+                        <div class="col-4">
+                            <a href="/To-do-listAgressive/router.php?action=logout">Выйти</a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-10 right-half">
